3fb45ef3222e39812bb2e70797662552f5211a5b
laalina <alina.beck@gmx.net>
2015-03-29 19:24:14 +0200

    cleaned the room.json

--- web/classes/room.dart
+++ web/classes/room.dart
@@ -2,81 +2,63 @@ library Room;
 
 import 'dart:html';
 import 'gamedata.dart';
+import 'item.dart';
 import 'furniture.dart';
 import 'gadget.dart';
 import 'door.dart';
 import 'canvas.dart';
 
 
-class Room {
-  String name;
-  Map roomItems;
-  List<Furniture> furniture = new List<Furniture>();
-  List<Gadget> gadgets = new List<Gadget>();
-  List<Door> doors = new List<Door>();
-  Map rooms = GameData.rooms;
+abstract class Room {
+  static String _name;
+  static Map _roomItems;
+  static List<Item> _items = new List<Item>();
+  static Map _rooms = GameData.rooms;
   
   
-  decorateRoom() {
+  static _decorateRoom() {
     
     // fill up with furniture
-    if (roomItems.containsKey('furniture')) {
-      roomItems['furniture'].forEach((k, v) {
-        furniture.add(new Furniture(name: k, posX: v['posX'], posY: v['posY']));
+    if (_roomItems.containsKey('furniture')) {
+      _roomItems['furniture'].forEach((k, v) {
+        _items.add(new Furniture(name: k, posX: v['posX'], posY: v['posY']));
       });
     }
     
     // fill up with gadgets
-    if (roomItems.containsKey('gadgets')) {
-      roomItems['gadgets'].forEach((k, v) {
-        gadgets.add(new Gadget(name: k, posX: v['posX'], posY: v['posY']));
+    if (_roomItems.containsKey('gadgets')) {
+      _roomItems['gadgets'].forEach((k, v) {
+        _items.add(new Gadget(name: k, posX: v['posX'], posY: v['posY']));
       });
     }
     
     // fill up with doors
-    if (roomItems.containsKey('doors')) {
-      int doorsLength = roomItems['doors'].length;
-            
-      for (int i = 0; i < doorsLength; i++) {
-        var d = roomItems['doors'][i];
-        doors.add(new Door(direction: d['direction'], posX: d['posX'], posY: d['posY']));
-      }
-      
+    if (_roomItems.containsKey('doors')) {
+      _roomItems['doors'].forEach((k, v) {
+        _items.add(new Door(direction: k, posX: v['posX'], posY: v['posY']));
+      });
     }
+  
     
-    drawRoom(Canvas.context);
+    _drawRoom(Canvas.context);
     
   }
   
   
-  drawRoom(CanvasRenderingContext2D context) {
-    int furnitureLength = furniture.length;
-    int gadgetsLength = gadgets.length;
-    int doorsLength = doors.length;
+  static _drawRoom(CanvasRenderingContext2D context) {
+    int itemsLength = _items.length;
     
-    for (int i = 0; i < furnitureLength; i++) {
-      Furniture f = furniture[i];
+    for (int i = 0; i < itemsLength; i++) {
+      Item it = _items[i];
       
-      context.strokeRect(f.posX, f.posY, f.width, f.height);
-      
-    }
-    for (int j = 0; j < gadgetsLength; j++) {
-      Gadget g = gadgets[j];
-      
-      context.strokeRect(g.posX, g.posY, g.width, g.height);
-      
-    }
-    for (int k = 0; k < doorsLength; k++) {
-      Door d = doors[k];
-     
-      context.strokeRect(d.posX, d.posY, d.width, d.height);
+      context.strokeRect(it.posX, it.posY, it.width, it.height);
     }
   }
   
-  Room(String roomName) {
-    this.name = roomName;
-    this.roomItems = rooms[roomName]['items'];
-    this.decorateRoom();
+  static setRoom(String roomName) {
+    _name = roomName;
+    _roomItems = _rooms[roomName]['items'];
+    _decorateRoom();
     
     }  
 }
